f6b8a0b87d0a7223b0c7f521f69be63fd663c89e
Baduk98 <142447944+Baduk98@users.noreply.github.com>
2023-12-05 18:16:34 +0300

    Update CashbackHackServiceTest.java
    add test for testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,3 +1,35 @@
 package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
+
+    CashbackHackService cash = new CashbackHackService();
+
+    @Test
+    public void testRemainLessThanBoundary() {
+        int amount = 500;
+        int expected = 500;
+        int actual = cash.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainEqualToBoundary() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = cash.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainGreaterThanBoundary() {
+        int amount = 1500;
+        int expected = 500;
+        int actual = cash.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
 }
