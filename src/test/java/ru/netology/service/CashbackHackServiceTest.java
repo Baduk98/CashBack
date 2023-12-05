d967ec2c08b55c5705b175bfd6f6df9ab4ae2a82
Baduk98 <142447944+Baduk98@users.noreply.github.com>
2023-12-05 18:23:42 +0300

    Update CashbackHackServiceTest.java
    add test for junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,3 +1,34 @@
 package ru.netology.service;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
+    CashbackHackService cash = new CashbackHackService();
+
+    @Test
+    public void testRemainLessThanBoundary() {
+        int amount = 500;
+        int expected = 500;
+        int actual = cash.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainEqualToBoundary() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = cash.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainGreaterThanBoundary() {
+        int amount = 1500;
+        int expected = 500;
+        int actual = cash.remain(amount);
+        assertEquals(expected, actual);
+    }
 }
